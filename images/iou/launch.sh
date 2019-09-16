1ab1ab2d0a95e62afebb745298ecff6643e7fc49
Jose Sanchez-Seco <jose.sanchez@satec.es>
2019-09-16 16:01:40 +0200

    prueba funcional /(leccion 65)

--- images/iou/launch.sh
+++ images/iou/launch.sh
@@ -70,43 +70,43 @@ random_mac () {
  -netdev \
  user,id=p00,net=10.0.0.0/24,tftp=/tftpboot,hostfwd=tcp::2022-10.0.0.15:22,hostfwd=udp::2161-10.0.0.15:161,hostfwd=tcp::2830-10.0.0.15:830,hostfwd=tcp::2880-10.0.0.15:8080 \
  -device \
- virtio-net-pci,netdev=vcp-int,mac=$(random_mac) \
+ e1000,netdev=vcp-int,mac=$(random_mac) \
  -netdev \
  tap,ifname=vcp-int,id=vcp-int,script=no,downscript=no \
  -device \
- virtio-net-pci,netdev=dummy0,mac=$(random_mac) \
+ e1000,netdev=dummy0,mac=$(random_mac) \
  -netdev \
  tap,ifname=dummy0,id=dummy0,script=no,downscript=no \
  -device \
- virtio-net-pci,netdev=p01,mac=$(random_mac),bus=pci.1,addr=0x2 \
+ e1000,netdev=p01,mac=$(random_mac),bus=pci.1,addr=0x2 \
  -netdev \
  tap,id=p01,ifname=tap0,script=no,downscript=no \
  -device \
- virtio-net-pci,netdev=p02,mac=$(random_mac),bus=pci.1,addr=0x3 \
+ e1000,netdev=p02,mac=$(random_mac),bus=pci.1,addr=0x3 \
  -netdev \
  tap,id=p02,ifname=tap1,script=no,downscript=no \
  -device \
- virtio-net-pci,netdev=p03,mac=$(random_mac),bus=pci.1,addr=0x4 \
+ e1000,netdev=p03,mac=$(random_mac),bus=pci.1,addr=0x4 \
  -netdev \
  tap,id=p03,ifname=tap2,script=no,downscript=no \
  -device \
- virtio-net-pci,netdev=p04,mac=$(random_mac),bus=pci.1,addr=0x5 \
+ e1000,netdev=p04,mac=$(random_mac),bus=pci.1,addr=0x5 \
  -netdev \
  tap,id=p04,ifname=tap3,script=no,downscript=no \
  -device \
- virtio-net-pci,netdev=p05,mac=$(random_mac),bus=pci.1,addr=0x6 \
+ e1000,netdev=p05,mac=$(random_mac),bus=pci.1,addr=0x6 \
  -netdev \
  tap,id=p05,ifname=tap4,script=no,downscript=no \
  -device \
- virtio-net-pci,netdev=p06,mac=$(random_mac),bus=pci.1,addr=0x7 \
+ e1000,netdev=p06,mac=$(random_mac),bus=pci.1,addr=0x7 \
  -netdev \
  tap,id=p06,ifname=tap5,script=no,downscript=no \
  -device \
- virtio-net-pci,netdev=p07,mac=$(random_mac),bus=pci.1,addr=0x8 \
+ e1000,netdev=p07,mac=$(random_mac),bus=pci.1,addr=0x8 \
  -netdev \
  tap,id=p07,ifname=tap6,script=no,downscript=no \
  -device \
- virtio-net-pci,netdev=p08,mac=$(random_mac),bus=pci.1,addr=0x9 \
+ e1000,netdev=p08,mac=$(random_mac),bus=pci.1,addr=0x9 \
  -netdev \
  tap,id=p08,ifname=tap7,script=no,downscript=no
 
