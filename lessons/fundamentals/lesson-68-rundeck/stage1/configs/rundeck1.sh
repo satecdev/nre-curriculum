e600e000df2c6a3bc7e59cd8b44d410139274f3b
khornem <khornem@gmail.com>
2019-09-21 09:46:54 +0200

    lesson66-68: pruebas con rest api de rundeck para la configuración inicial

---
+++ lessons/fundamentals/lesson-68-rundeck/stage1/configs/rundeck1.sh
@@ -0,0 +1,27 @@
+!#/bin/bash
+
+STAGE="stage1"
+
+#-- Get key
+curl -D - -X POST -H "Content-Type: application/x-www-form-urlencoded"   -H "Cache-Control: no-cache"   -d "j_username=admin&j_password=admin"   --cookie-jar /antidote/${STAGE}/configs/rd_cookie   http://localhost:4440/${SYRINGE_FULL_REF}/j_security_check
+
+#-- delete all projects
+
+# delete all projects
+#curl -s -X GET -H "Accept: application/json" --cookie /antidote/${STAGE}/configs/rd_cookie "http://localhost:4440/${SYRINGE_FULL_REF}/api/32/projects" | jq -r ' .[] | .name' | while read project
+#do
+#    curl -vvv -X DELETE -H "Accept: application/json"  --cookie /antidote/${STAGE}/configs/rd_cookie "http://localhost:4440/${SYRINGE_FULL_REF}/api/32/project/${project}"
+#done
+curl -vvv -X DELETE -H "Accept: application/json"  --cookie /antidote/${STAGE}/configs/rd_cookie "http://localhost:4440/${SYRINGE_FULL_REF}/api/32/project/SATEC"
+
+
+#-- Create project. PENDING QUOTES FORMAT
+curl -vvv -X POST -H "Content-Type: application/json" -d {"name":"SATEC","config":{"project.label":"SATEC-label","resources.source.2.type":"file","resources.source.2.config.file":"/antidote/${STAGE}/configs/rundeckinventory.json","resources.source.2.config.format":"resourcejson"}} --cookie /antidote/stage1/configs/rd_cookie "http://localhost:4440/${SYRINGE_FULL_REF}/api/32/projects"
+#curl -vvv -X POST -H "Content-Type: application/json" -d '{ "name": "SATEC", "config": { "project.label":"SATEC-label", "resources.source.2.type":"file", "resources.source.2.config.file":"/antidote/stage1/configs/rundeckinventory.json", "resources.source.2.config.format":"resourcejson" } }' --cookie /antidote/stage1/configs/rd_cookie "http://localhost:4440/${SYRINGE_FULL_REF}/api/32/projects"
+
+#-- add key 
+curl -vvv -X POST -H "Content-type: application/x-rundeck-data-password" -d "antidotepassword" --cookie /antidote/${STAGE}/configs/rd_cookie "http://localhost:4440/${SYRINGE_FULL_REF}/api/32/storage/keys/antidote/cpe"
+
+#-- create jobs
+curl -vvv -X POST -H "Content-Type: application/yaml" --data-binary @/antidote/${STAGE}/configs/rundeck.jobs.yml --cookie /antidote/${STAGE}/configs/rd_cookie "http://localhost:4440/${SYRINGE_FULL_REF}/api/32/project/SATEC/jobs/import?fileformat=yaml&dupeOption=update"
+
