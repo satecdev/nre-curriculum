f1ae5f6b550c14a19869b4515309f26cb29f7bd6
khornem <khornem@gmail.com>
2019-09-23 21:36:02 +0200

    satec:lesson66:use rundeck.sh

---
+++ lessons/fundamentals/lesson-66-pe_provision/rundeck_config/generate_jobs.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+# stage 1 jobs
+
+rm -rf stage1/
+rundeck_jobs -i jobs_lesson1_multiple.yml  -d stage1 -u 00000000-0000-0000-0000-000000000000 -l debug
+
+
+# stage 2 jobs
+rm -rf stage2/
+rundeck_jobs -i jobs_stage2_config.yml -d stage2 -u 00000000-0000-0000-0000-000000000000 -l debug
+
+rundeck_jobs -i jobs_stage2_verifications_ios1.yml -d stage2 -u 00000000-0000-0000-0000-000000000000 -o 101 -l debug -s "ios1: l3vpn configuration verification at ios1"
+rundeck_jobs -i jobs_stage2_verifications_ios1_ios2.yml -d stage2 -u 00000000-0000-0000-0000-000000000000 -o 102 -l debug -s "ios1: l3vpn configuration verification at ios2-rr"
+rundeck_jobs -i jobs_stage2_verifications_ios1_vqfx3.yml -d stage2 -u 00000000-0000-0000-0000-000000000000 -o 103 -l debug -s "ios1: l3vpn configuration verification at vqfx3-rr"
+
+rundeck_jobs -i jobs_stage2_verifications_ios2.yml -d stage2 -u 00000000-0000-0000-0000-000000000000 -o 201 -l debug -s "ios2: l3vpn configuration verification at ios2"
+rundeck_jobs -i jobs_stage2_verifications_ios1_vqfx3.yml -d stage2 -u 00000000-0000-0000-0000-000000000000 -o 202 -l debug -s "ios2: l3vpn configuration verification vqfx3-rr"
+
+
+rundeck_jobs -i jobs_stage2_verifications_ios4.yml -d stage2 -u 00000000-0000-0000-0000-000000000000 -o 301 -l debug -s "ios4: l3vpn configuration verification at ios4"
+rundeck_jobs -i jobs_stage2_verifications_ios4_ios2.yml -d stage2 -u 00000000-0000-0000-0000-000000000000 -o 302 -l debug -s "ios4: l3vpn configuration verification at ios2-rr"
+rundeck_jobs -i jobs_stage2_verifications_ios4_vqfx3.yml -d stage2 -u 00000000-0000-0000-0000-000000000000 -o 303 -l debug -s "ios4: l3vpn configuration verification at vqfx3-rr"
+
+
+rundeck_jobs -i jobs_stage2_verifications_connectivity.yml -d stage2 -u 00000000-0000-0000-0000-000000000000 -o 400 -l debug -s "end to end connectivity tests"
+
+rm stage2/rundeck.jobs.yml
+for f in stage2/*.yml
+do
+ cat $f >> stage2/rundeck.jobs.yml
+done
+
+#stage 3
+rm -rf stage3/
+
+rundeck_jobs -i jobs_tests_ios1.yml -d stage3 -u 00000000-0000-0000-0000-000000000000 -o 401 -l debug -s "IOS1:_TROUBLESHOOTING_TESTS"
+rundeck_jobs -i jobs_tests_ios2.yml -d stage3 -u 00000000-0000-0000-0000-000000000000 -o 402 -l debug -s "IOS2:_TROUBLESHOOTING_TESTS"
+rundeck_jobs -i jobs_tests_ios4.yml -d stage3 -u 00000000-0000-0000-0000-000000000000 -o 403 -l debug -s "IOS4:_TROUBLESHOOTING_TESTS"
+rm stage3/rundeck.jobs.yml
+
+for f in stage3/*.yml
+do
+ cat $f >> stage3/rundeck.jobs.yml
+done
