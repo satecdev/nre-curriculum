e2dffcb1810a115ca96903889a08426eef7b9f44
khornem <khornem@gmail.com>
2019-09-23 20:22:12 +0200

    satec:lesson66: add stage2 verifications

--- lessons/fundamentals/lesson-66-pe_provision/stage1/configs/rundeck.sh
+++ lessons/fundamentals/lesson-66-pe_provision/stage1/configs/rundeck.sh
@@ -18,7 +18,7 @@ curl -vvv -X DELETE -H "Accept: application/json"  --cookie /home/antidote/rd_co
 
 #-- Create project. PENDING QUOTES FORMAT in order to use variable STAGE
 #curl -vvv -X POST -H "Content-Type: application/json" -d {"name":"SATEC","config":{"project.label":"SATEC-label","resources.source.2.type":"file","resources.source.2.config.file":"/home/antidote/rundeckinventory.json","resources.source.2.config.format":"resourcejson"}} --cookie /home/antidote/rd_cookie "http://localhost:4440/${SYRINGE_FULL_REF}/api/32/projects"
-curl -vvv -X POST -H "Content-Type: application/json" -d '{ "name": "SATEC", "config": { "project.label":"SATEC-label", "resources.source.2.type":"file", "resources.source.2.config.file":"/home/antidote/rundeckinventory.json", "resources.source.2.config.format":"resourcejson" } }' --cookie /home/antidote/rd_cookie "http://localhost:4440/${SYRINGE_FULL_REF}/api/32/projects"
+curl -vvv -X POST -H "Content-Type: application/json" -d '{ "name": "SATEC", "config": { "project.label":"SATEC-label", "resources.source.2.type":"file", "resources.source.2.config.file":"/antidote/rundeck_config/rundeckinventory.json", "resources.source.2.config.format":"resourcejson" } }' --cookie /home/antidote/rd_cookie "http://localhost:4440/${SYRINGE_FULL_REF}/api/32/projects"
 
 #-- add key 
 curl -vvv -X POST -H "Content-type: application/x-rundeck-data-password" -d "antidotepassword" --cookie /home/antidote/rd_cookie "http://localhost:4440/${SYRINGE_FULL_REF}/api/32/storage/keys/antidote/cpe"
